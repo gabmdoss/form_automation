232488890e9c54ca363b85a54c06511b60816db6
gabmdoss <gabmdoss@gmail.com>
2023-02-08 14:47:41 -0300

    Adição de steps form

--- features/page_objects/form_automation.rb
+++ features/page_objects/form_automation.rb
@@ -1,16 +1,62 @@
 class Form < SitePrism::Page
-    set_url 'https://demo.automationtesting.in/Register.html'
+    set_url 'https://demoqa.com/automation-practice-form'
 
-    element :first_name_input, :xpath, '//*[@id="basicBootstrapForm"]/div[1]/div[1]/input'
-    element :last_name_input, :xpath, '//*[@id="basicBootstrapForm"]/div[1]/div[2]/input'
+    element :first_name_input, 'input[placeholder="First Name"]'
+    element :last_name_input, 'input[placeholder="Last Name"]'
+    element :email_input, 'input[placeholder="name@example.com"]'
+    element :gender_btn, 'label[for="gender-radio-2"]'
+    element :mobile_input, 'input[placeholder="Mobile Number"]'
+    element :date_birth_input, 'input[id="dateOfBirthInput"]'
+    element :street_address_input, 'textarea[placeholder="Current Address"]'
+    element :register_btn, 'button[id="submit"]'
 
     def first_name
-        name = Faker::Name.first_name
-        first_name_input.set(name)
+        @name = Faker::Name.female_first_name
+        first_name_input.set(@name)
     end
     
     def last_name
-        name = Faker::Name.last_name
-        last_name_input.set(name)
+        last_name_input.set(Faker::Name.last_name)
+    end
+
+    def email
+        t = date_time.gsub!(/[:.]/,' ')
+        email = "#{@name}#{t.delete!(' ')}" + "@gabautomation.com"
+        email_input.set(email.downcase)
+    end
+
+    def gender
+        gender_btn.click
+    end
+
+    def mobile
+        mobile_input.set(Faker::PhoneNumber.subscriber_number(length: 8))
+    end
+
+    def date_birth
+        date_birth_input.set(Faker::Date.between(from: '1920-01-01', to: '2005-01-01'))
+    end
+
+    def hobbies
+        array = ['Sports', 'Reading', 'Music'].sample
+        Capybara.current_session.find(:xpath, "//*[text() = '#{array}']", wait: 5)
+    end
+
+    def street_address
+        street_address_input.set(Faker::Address.street_address)
+    end
+
+    def state
+        find('#state').click
+        Capybara.current_session.find(:xpath, "//*[text() = 'NCR']", wait: 5).click
+    end
+
+    def city
+        find('#city').click
+        Capybara.current_session.find(:xpath, "//*[text() = 'Delhi']", wait: 5).click
+    end
+
+    def register
+        register_btn.click
     end
 end
--- features/step_definitions/form_automation_steps.rb
+++ features/step_definitions/form_automation_steps.rb
@@ -3,7 +3,25 @@ Dado('que cliente acessa formulário de cadastro') do
     @form.load
 end
   
-Quando('inserir os dados de registro') do
+Quando('inserir os dados de cadastro') do
     @form.first_name
     @form.last_name
+    @form.email
+    @form.gender
+    @form.mobile
+    @form.date_birth
+    @form.hobbies
+    @form.street_address
+    @form.state
+    @form.city
 end
+
+Quando('submeter o registro') do
+    @form.register
+    #vai gerar erro pois o site está com bug no botão 'submit'
+end
+  
+Então('é realizado o registro do cliente') do
+    #qa não sabe qual é a página após registrar
+end
+  
